b733050a5de40b4c056062eac23ebafde7233ae2
Khalid Gad <107983594+imkhalidgad@users.noreply.github.com>
2024-09-06 15:33:03 +0300

    Initial Commit

---
+++ Foody/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Foody
+//
+//  Created by Khalid Gad on 06/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
